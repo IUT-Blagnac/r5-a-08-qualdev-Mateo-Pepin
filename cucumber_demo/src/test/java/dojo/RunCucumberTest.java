7080811ad7c177c12ef59df10f356946d479f4dc
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-12-13 12:47:16 +0100

    jpp

---
+++ cucumber_demo/src/test/java/dojo/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package dojo;
+
+import cucumber.api.junit.Cucumber;
+import dojo.CocktailSteps;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)  
+public class RunCucumberTest {
+}
